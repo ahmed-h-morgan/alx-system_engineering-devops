0e543ac5c8b3f2f186685cd13f293c9736ae9990
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-18 06:37:44 +0200

    try to create puppet file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# a puppet to create a file 
+
+file { '/temp/school':
+  ensure  => 'present'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
