ff00f7f574e21c288be64f04a679f1726abb951f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-19 22:23:27 +0200

    update 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,8 +2,8 @@
 package { 'flask':
   ensure          => '2.1.0',
   provider        => 'pip3',
-  command         => 'sudo apt install python3-pip'
-  command         => 'pip3 install flask',
+  command         => 'sudo apt install python3-pip | pip3 install flask',
+  # command         => 'pip3 install flask',
   # install_options => [
   #   { '--index-url' => 'https://pypi.mycorp.com' },
   # ]
