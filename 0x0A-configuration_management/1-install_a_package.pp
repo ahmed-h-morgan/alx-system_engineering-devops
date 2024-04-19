10ca2eabbc840714c40b822e80786f063b91519e
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-19 21:54:05 +0200

    try - task 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Using Puppet, install flask from pip3.
 package { 'flask':
-  
-
+  ensure          => 'installed',
+  provider        => 'pip3',
+  install_options => [
+    { '--index-url' => 'https://pypi.mycorp.com' },
+  ]
 }
